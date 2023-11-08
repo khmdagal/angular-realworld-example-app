20249bbaeddda0bff1141c3db6aa140213aed997
khmdagal <khmdagal@gmail.com>
2023-11-08 11:53:09 +0000

    Article and assertion

--- cypress/e2e/login.spec.js
+++ cypress/e2e/login.spec.js
@@ -1,3 +1,5 @@
+/// <reference types="cypress" />
+
 describe("template spec", () => {
   beforeEach("First log in the app", () => {
     cy.visit("/login");
@@ -6,17 +8,23 @@ describe("template spec", () => {
     cy.get("form").submit();
   });
 
-
   it("Create article", () => {
+    cy.intercept("POST", "https://api.realworld.io/api/articles/").as(
+      "postArticles"
+    );
 
-
-    cy.contains('New Article').click()
-    cy.get('[formcontrolname="title"]').type('Ciwaanka')
+    cy.contains("New Article").click();
+    cy.get('[formcontrolname="title"]').type("Ciwaanka");
     cy.get('[formcontrolname="description"]').type("maduuca");
     cy.get('[formcontrolname="body"]').type("sheekooy sheeko xariira");
-    cy.contains('Publish').click()
+    cy.contains("Publish").click();
+
+    cy.wait("@postArticles");
+    cy.get("@postArticles").then((xhr) => {
+      console.log("=====>>>>>", xhr);
+      expect(xhr.response.statusCode).to.equal(201);
+      expect(xhr.request.body.article.body).to.equal("sheekooy sheeko xariira");
+    });
   });
-  it.only("test", () => {
-    
-  })
+  // it.only("test", () => {});
 });
