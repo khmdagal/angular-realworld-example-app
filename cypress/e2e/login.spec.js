23f894b7cdf9338a7a953a90624055c27eaf4c2e
khmdagal <khmdagal@gmail.com>
2023-11-10 10:36:44 +0000

    verify global feed likes counts

--- cypress/e2e/login.spec.js
+++ cypress/e2e/login.spec.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-describe("template spec", () => {
+describe("Interceptions", () => {
 
   // since we want to intercept tags and tags are the first thing to load when the page is logged in 
   // we wan to do the interception before the login happen, so we can able to provide our mock response
@@ -31,12 +31,41 @@ cy.intercept('GET','https://api.realworld.io/api/tags', {fixture: 'tags.json'})
     });
   });
 
-  it.only("intercepting with mock response", () => {
+  it("intercepting with mock response", () => {
     // know we are doing assertions
     cy.get(".tag-list")
       .should("contain", "Computer")
       .should("contain", "JavaScript")
       .should("contain", "Code Your Future");
-    
   });
+
+  it.only('verify global feed likes counts', () => {
+    //here we are intercepting the both feed your feed and the global one
+    //first we are doing your feed intercept
+    cy.intercept("GET", "https://api.realworld.io/api/articles/feed*", {
+      articles: [],
+      articlesCount: 0,
+    });
+
+
+    // in here we intercept the global feed and provide our own response from the file we created in the fixture folder
+    // the file name is articles.json
+    cy.intercept('GET', "https://api.realworld.io/api/articles*", { fixture: 'articles.json' })
+    
+    // here we are going to find and click the Global feed button on the home page 
+    
+    cy.contains('Global Feed').click();
+
+    // Then we are doing to get all the article list or the parent element of all the articles
+    // and then child buttons 
+    
+    cy.get("app-article-list button").then(heartButtonList => {
+
+      // then we are looking the first and the second buttons
+      // then we are doing the assertions if you look the articles.json file we provide 1 and 5 favorite counts.
+      expect(heartButtonList[0]).to.contain('1');
+      expect(heartButtonList[1]).to.contain("5");
+
+    })
+  })
 });
